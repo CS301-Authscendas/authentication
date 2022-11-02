5b9593edbc8ea042fe239dac77912be096e27673
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-11-03 01:35:22 +0800

    fix(auth): test cases

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,6 +2,7 @@ import { HttpModule } from "@nestjs/axios";
 import { ConfigModule } from "@nestjs/config";
 import { PassportModule } from "@nestjs/passport";
 import { Test, TestingModule } from "@nestjs/testing";
+import { KmsModule } from "../kms/kms.module";
 import { NotificationModule } from "../notification/notification.module";
 import { OrganizationModule } from "../organization/organization.module";
 import { UserModule } from "../user/user.module";
@@ -14,7 +15,15 @@ describe("AuthController", () => {
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [ConfigModule, HttpModule, UserModule, NotificationModule, OrganizationModule, PassportModule],
+            imports: [
+                ConfigModule,
+                HttpModule,
+                UserModule,
+                NotificationModule,
+                OrganizationModule,
+                PassportModule,
+                KmsModule,
+            ],
             controllers: [AuthController],
             providers: [AuthService, LoginStrategy],
         }).compile();
